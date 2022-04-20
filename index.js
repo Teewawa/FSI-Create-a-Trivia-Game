f327f700740bb8a450581723a1c465a09acfa8c3
Teewawa <dev.tiaram@gmail.com>
2022-04-20 16:30:12 -0700

    Adding to the index.js - Created function to play game, display user score at the end, and asks user if they want to play again. If yes, game restarts, if no a prompt appears

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your Code Here
+//Get the user's name
+let userName = window.prompt("Please enter your name.");
+let userScore = 0;
+let playAgain = true;
+
+//Function to play game
+function playGame() {
+  //Prompt the user with each question
+  for (let i = 0; i < questions.length; i++) {
+    console.log(questions[i]);
+    let question = questions[i];
+    let userAnswer = window.prompt(question.text);
+    if (userAnswer === question.correctAnswer) {
+      userScore = userScore + 10;
+      console.log("Correct");
+    }
+  }
+  window.alert("Your score is: " + userScore);
+}
+while (playAgain === true) {
+  playGame();
+  let userChoice = window.prompt(
+    "Would you like to play the game again? Answer yes or no"
+  );
+  if (userChoice === "yes") {
+    playAgain = true;
+  } else {
+    playAgain = false;
+    window.alert("Thanks for playing the game!");
+  }
+}
